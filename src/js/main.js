13d19a04f9f298d053fec5cf031f344dc91c17ab
Pablo Aznárez <paznarez@ficodes.com>
2020-03-17 17:27:30 +0100

    Added license headers to js files

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,17 @@
 /*
- * generic-ngsimodel
- * https://github.com/pablo/generic-ngsimodel-operator
+ * Copyright (c) 2020 Future Internet Consulting and Development Solutions S.L.
  *
- * Copyright (c) 2020 Vendor
- * Licensed under the MIT license.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 'use strict'
 
